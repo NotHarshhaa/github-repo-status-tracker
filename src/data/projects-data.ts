732f311c28efb32c5800c212a772bec641159060
NotHarshhaa <NotHarshhaa@users.noreply.github.com>
2026-04-21 13:45:49 +0000

    git-tracker: 🔄 Auto-update repository status [skip ci]

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -148,10 +148,10 @@ export const PROJECTS: Project[] = [
       "label": "github.com/NotHarshhaa/DevOps-Projects",
       "href": "https://github.com/NotHarshhaa/DevOps-Projects"
     },
-    "stars": 3929,
+    "stars": 3930,
     "forks": 3934,
     "issues": 20,
-    "lastUpdated": "2026-04-21T05:43:27Z",
+    "lastUpdated": "2026-04-21T13:05:10Z",
     "lastCommit": "2c7404166f5c37229dcca56b35818086bc0b4d40"
   },
   {
@@ -433,10 +433,10 @@ export const PROJECTS: Project[] = [
       "label": "github.com/NotHarshhaa/DevOps-Interview-Questions",
       "href": "https://github.com/NotHarshhaa/DevOps-Interview-Questions"
     },
-    "stars": 833,
+    "stars": 832,
     "forks": 615,
     "issues": 2,
-    "lastUpdated": "2026-04-21T07:17:03Z",
+    "lastUpdated": "2026-04-21T13:05:33Z",
     "lastCommit": "4dc7fb875a13d34cd91071005cabd414f0d65166"
   },
   {
